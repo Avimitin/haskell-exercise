1fd7665da681d1cb21e95601a6efb9cf0d8aae9f
Avimitin <dev@avimit.in>
2023-05-11 20:26:57 +0800

    CIS194: Homework2 - Finish the homework2
    Signed-off-by: Avimitin <dev@avimit.in>

--- CIS194/homework2/LogAnalysis.hs
+++ CIS194/homework2/LogAnalysis.hs
@@ -27,3 +27,43 @@ parseErrorLog [] = Unknown "Unreachable"
 
 toInt :: String -> Int
 toInt = read
+
+newMsgTreeNode :: LogMessage -> MessageTree
+newMsgTreeNode msg = Node Leaf msg Leaf
+
+insert :: LogMessage -> MessageTree -> MessageTree
+insert (Unknown _) _ = Leaf
+insert msg Leaf = newMsgTreeNode msg
+insert _ (Node _ (Unknown _) _) = Leaf
+insert msg@(LogMessage _ t _) (Node lchild node@(LogMessage _ cur_t _) Leaf) =
+  if t > cur_t
+    then Node lchild node (newMsgTreeNode msg)
+    else Node (insert msg lchild) node Leaf
+insert msg@(LogMessage _ t _) (Node Leaf node@(LogMessage _ cur_t _) rchild) =
+  if t > cur_t
+    then Node Leaf node (insert msg rchild)
+    else Node (newMsgTreeNode msg) node rchild
+insert msg@(LogMessage _ t _) (Node lchild node@(LogMessage _ cur_t _) rchild) =
+  if t > cur_t
+    then Node lchild node (insert msg rchild)
+    else Node (insert msg lchild) node rchild
+
+build :: [LogMessage] -> MessageTree
+build = foldr insert Leaf
+
+inOrder :: MessageTree -> [LogMessage]
+inOrder Leaf = []
+inOrder (Node lchild msg rchild) = inOrder lchild ++ [msg] ++ inOrder rchild
+
+isError :: LogMessage -> Bool
+isError msg = case msg of
+  LogMessage (Error level) _ _ | level > 50 -> True
+  _ -> False
+
+unwrapError :: LogMessage -> String
+unwrapError msg = case msg of
+  LogMessage (Error _) _ text -> text
+  _ -> "Unreachable"
+
+whatWentWrong :: [LogMessage] -> [String]
+whatWentWrong = map unwrapError . inOrder . build . filter isError
